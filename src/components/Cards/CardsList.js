432cd7aa337e65a200392441e91f89490bc3c9fb
ahmedameenaim <run_2005_2005@yahoo.com>
2018-06-17 06:32:21 +0200

    first commit

---
+++ src/components/Cards/CardsList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Card from './Cards'
+
+const CardsList = ({robots}) => {
+    
+    const cardComponent = robots.map((user,i) => {
+        return (
+        <Card key = {i}
+        id = {robots[i].id} 
+        name = {robots[i].name} 
+        username = {robots[i].username} 
+        email = {robots[i].email} />);
+    })
+    return (
+    <div>
+    {cardComponent}
+    </div>
+    );
+}
+
+
+export default CardsList;
